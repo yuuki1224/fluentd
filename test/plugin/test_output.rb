0987ef4965c4b5c85df4b1d86d36bbdebf17f15d
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-04-12 16:28:22 +0900

    writing tests

--- test/plugin/test_output.rb
+++ test/plugin/test_output.rb
@@ -2,6 +2,17 @@ require_relative '../helper'
 require 'fluent/plugin/output'
 require 'fluent/plugin/buffer'
 
+def create_output(type=:full)
+  case type
+  when :bare     then FluentPluginOutputTest::DummyBareOutput.new
+  when :sync     then FluentPluginOutputTest::DummySyncOutput.new
+  when :buffered then FluentPluginOutputTest::DummyAsyncOutput.new
+  when :delayed  then FluentPluginOutputTest::DummyDelayedOutput.new
+  when :full     then FluentPluginOutputTest::DummyFullFeatureOutput.new
+  else
+    raise ArgumentError, "unknown type: #{type}"
+  end
+end
 def create_metadata(timekey: nil, tag: nil, variables: nil)
   Fluent::Plugin::Buffer::Metadata.new(timekey, tag, variables)
 end
@@ -14,21 +25,26 @@ module FluentPluginOutputTest
     end
   end
   class DummyAsyncOutput < Fluent::Plugin::Output
+    attr_accessor :formatter
     def format(tag, time, record)
+      @formatter ? @formatter.call(tag, time, record) : ''
     end
     def write(chunk)
     end
   end
   class DummyDelayedOutput < Fluent::Plugin::Output
+    attr_accessor :formatter
     def format(tag, time, record)
+      @formatter ? @formatter.call(tag, time, record) : ''
     end
     def try_write(chunk)
     end
   end
   class DummyFullFeatureOutput < Fluent::Plugin::Output
-    attr_accessor :prefer_buffered, :prefer_delayed
+    attr_accessor :prefer_buffered, :prefer_delayed, :formatter
     def initialize
       super
+      @formatter = nil
       @prefer_buffered = @prefer_delayed = false
     end
     def prefer_buffered_processing
@@ -40,6 +56,7 @@ module FluentPluginOutputTest
     def process(tag, es)
     end
     def format(tag, time, record)
+      @formatter ? @formatter.call(tag, time, record) : ''
     end
     def write(chunk)
     end
@@ -180,11 +197,247 @@ class OutputTest < Test::Unit::TestCase
       assert_equal "/mypath/2016/04/11/20-30/fluentd.test.output/////tail", @i.extract_placeholders(tmpl, m)
     end
 
-    test '#metadata returns object which contains tag/timekey/variables from records as specified in configuration'
-    test '#emit calls #emit_sync for non-buffered output'
-    test '#emit calls #emit_buffered for buffered output'
-    test '#prefer_buffered_processing decides buffered or not without <buffer> section'
-    test '#prefer_delayed_commit decides delayed commit is enabled or not if both are implemented'
+    test '#metadata returns object which contains tag/timekey/variables from records as specified in configuration' do
+      tag = 'test.output'
+      time = Time.parse('2016-04-12 15:31:23 -0700').to_i
+      timekey = Time.parse('2016-04-12 15:00:00 -0700').to_i
+      record = {"key1" => "value1", "num1" => 1, "message" => "my message"}
+
+      i1 = create_output(:buffered)
+      i1.configure(config_element('ROOT','',{},[config_element('buffer', '')]))
+      assert_equal create_metadata(), i1.metadata(tag, time, record)
+
+      i2 = create_output(:buffered)
+      i2.configure(config_element('ROOT','',{},[config_element('buffer', 'tag')]))
+      assert_equal create_metadata(tag: tag), i2.metadata(tag, time, record)
+
+      i3 = create_output(:buffered)
+      i3.configure(config_element('ROOT','',{},[config_element('buffer', 'time', {"timekey_range" => 3600, "timekey_zone" => "-0700"})]))
+      assert_equal create_metadata(timekey: timekey), i3.metadata(tag, time, record)
+
+      i4 = create_output(:buffered)
+      i4.configure(config_element('ROOT','',{},[config_element('buffer', 'key1', {})]))
+      assert_equal create_metadata(variables: {key1: "value1"}), i4.metadata(tag, time, record)
+
+      i5 = create_output(:buffered)
+      i5.configure(config_element('ROOT','',{},[config_element('buffer', 'key1,num1', {})]))
+      assert_equal create_metadata(variables: {key1: "value1", num1: 1}), i5.metadata(tag, time, record)
+
+      i6 = create_output(:buffered)
+      i6.configure(config_element('ROOT','',{},[config_element('buffer', 'tag,time', {"timekey_range" => 3600, "timekey_zone" => "-0700"})]))
+      assert_equal create_metadata(timekey: timekey, tag: tag), i6.metadata(tag, time, record)
+
+      i7 = create_output(:buffered)
+      i7.configure(config_element('ROOT','',{},[config_element('buffer', 'tag,num1', {"timekey_range" => 3600, "timekey_zone" => "-0700"})]))
+      assert_equal create_metadata(tag: tag, variables: {num1: 1}), i7.metadata(tag, time, record)
+
+      i8 = create_output(:buffered)
+      i8.configure(config_element('ROOT','',{},[config_element('buffer', 'time,tag,key1', {"timekey_range" => 3600, "timekey_zone" => "-0700"})]))
+      assert_equal create_metadata(timekey: timekey, tag: tag, variables: {key1: "value1"}), i8.metadata(tag, time, record)
+    end
+
+    test '#emit calls #process via #emit_sync for non-buffered output' do
+      i = create_output(:sync)
+      process_called = false
+      (class << i; self; end).module_eval do
+        define_method(:process){ |tag, es|
+          process_called = true
+        }
+      end
+      i.configure(config_element())
+      i.start
+
+      t = Time.now.to_i
+      i.emit('tag', [ [t, {"key" => "value1"}], [t, {"key" => "value2"}] ])
+
+      assert process_called
+
+      i.stop; i.before_shutdown; i.shutdown; i.after_shutdown; i.close; i.terminate
+    end
+
+    test '#emit calls #format for buffered output' do
+      i = create_output(:buffered)
+      format_called_times = 0
+      (class << i; self; end).module_eval do
+        define_method(:format){ |tag, time, record|
+          format_called_times += 1
+          ''
+        }
+      end
+      i.configure(config_element())
+      i.start
+
+      t = Time.now.to_i
+      i.emit('tag', [ [t, {"key" => "value1"}], [t, {"key" => "value2"}] ])
+
+      assert_equal 2, format_called_times
+
+      i.stop; i.before_shutdown; i.shutdown; i.after_shutdown; i.close; i.terminate
+    end
+
+    test '#prefer_buffered_processing (returns false) decides non-buffered without <buffer> section' do
+      i = create_output(:full)
+
+      process_called = false
+      (class << i; self; end).module_eval do
+        define_method(:process){ |tag, es|
+          process_called = true
+        }
+      end
+      format_called_times = 0
+      (class << i; self; end).module_eval do
+        define_method(:format){ |tag, time, record|
+          format_called_times += 1
+          ''
+        }
+      end
+
+      i.configure(config_element())
+      i.prefer_buffered = false # delayed decision is possible to change after (output's) configure
+      i.start
+
+      assert !i.prefer_buffered
+      assert !i.prefer_buffered_processing
+
+      t = Time.now.to_i
+      i.emit('tag', [ [t, {"key" => "value1"}], [t, {"key" => "value2"}] ])
+
+      assert process_called
+      assert_equal 0, format_called_times
+
+      i.stop; i.before_shutdown; i.shutdown; i.after_shutdown; i.close; i.terminate
+    end
+
+    test '#prefer_buffered_processing (returns true) decides buffered without <buffer> section' do
+      i = create_output(:full)
+
+      process_called = false
+      (class << i; self; end).module_eval do
+        define_method(:process){ |tag, es|
+          process_called = true
+        }
+      end
+      format_called_times = 0
+      (class << i; self; end).module_eval do
+        define_method(:format){ |tag, time, record|
+          format_called_times += 1
+          ''
+        }
+      end
+
+      i.configure(config_element())
+      i.prefer_buffered = true # delayed decision is possible to change after (output's) configure
+      i.start
+
+      assert i.prefer_buffered
+      assert i.prefer_buffered_processing
+
+      t = Time.now.to_i
+      i.emit('tag', [ [t, {"key" => "value1"}], [t, {"key" => "value2"}] ])
+
+      assert !process_called
+      assert_equal 2, format_called_times
+
+      i.stop; i.before_shutdown; i.shutdown; i.after_shutdown; i.close; i.terminate
+    end
+
+    test 'output plugin will call #write for normal buffered plugin to flush buffer chunks' do
+      i = create_output(:buffered)
+      i.formatter = ->(tag, time, record){ [tag,time,record].to_json }
+      write_called = false
+      (class << i; self; end).module_eval do
+        define_method(:write){ |chunk| write_called = true }
+      end
+
+      i.configure(config_element('ROOT', '', {}, [config_element('buffer', '', {"flush_mode" => "immediate"})]))
+      i.start
+
+      t = Time.now.to_i
+      i.emit('tag', [ [t, {"key" => "value1"}], [t, {"key" => "value2"}] ])
+      i.force_flush
+
+      assert write_called
+
+      i.stop; i.before_shutdown; i.shutdown; i.after_shutdown; i.close; i.terminate
+    end
+
+    test 'output plugin will call #try_write for plugin supports delayed commit only to flush buffer chunks' do
+      i = create_output(:delayed)
+      i.formatter = ->(tag, time, record){ [tag,time,record].to_json }
+      try_write_called = false
+      (class << i; self; end).module_eval do
+        define_method(:try_write){ |chunk| try_write_called = true; commit_write(chunk.unique_id) }
+      end
+
+      i.configure(config_element('ROOT', '', {}, [config_element('buffer', '', {"flush_mode" => "immediate"})]))
+      i.start
+
+      t = Time.now.to_i
+      i.emit('tag', [ [t, {"key" => "value1"}], [t, {"key" => "value2"}] ])
+      i.force_flush
+
+      assert try_write_called
+
+      i.stop; i.before_shutdown; i.shutdown; i.after_shutdown; i.close; i.terminate
+    end
+
+    test '#prefer_delayed_commit (returns false) decides delayed commit is disabled if both are implemented' do
+      i = create_output(:full)
+      i.formatter = ->(tag, time, record){ [tag,time,record].to_json }
+      write_called = false
+      try_write_called = false
+      (class << i; self; end).module_eval do
+        define_method(:write){ |chunk| write_called = true }
+      end
+      (class << i; self; end).module_eval do
+        define_method(:try_write){ |chunk| try_write_called = true; commit_write(chunk.unique_id) }
+      end
+
+      i.configure(config_element('ROOT', '', {}, [config_element('buffer', '', {"flush_mode" => "immediate"})]))
+      i.prefer_delayed = false # delayed decision is possible to change after (output's) configure
+      i.start
+
+      assert !i.prefer_delayed
+      assert !i.prefer_delayed_commit
+
+      t = Time.now.to_i
+      i.emit('tag', [ [t, {"key" => "value1"}], [t, {"key" => "value2"}] ])
+      i.force_flush
+
+      assert write_called
+      assert !try_write_called
+
+      i.stop; i.before_shutdown; i.shutdown; i.after_shutdown; i.close; i.terminate
+    end
+
+    test '#prefer_delayed_commit (returns true) decides delayed commit is enabled if both are implemented' do
+      i = create_output(:full)
+      i.formatter = ->(tag, time, record){ [tag,time,record].to_json }
+      write_called = false
+      try_write_called = false
+      (class << i; self; end).module_eval do
+        define_method(:write){ |chunk| write_called = true }
+      end
+      (class << i; self; end).module_eval do
+        define_method(:try_write){ |chunk| try_write_called = true; commit_write(chunk.unique_id) }
+      end
+
+      i.configure(config_element('ROOT', '', {}, [config_element('buffer', '', {"flush_mode" => "immediate"})]))
+      i.prefer_delayed = true # delayed decision is possible to change after (output's) configure
+      i.start
+
+      assert i.prefer_delayed
+      assert i.prefer_delayed_commit
+
+      t = Time.now.to_i
+      i.emit('tag', [ [t, {"key" => "value1"}], [t, {"key" => "value2"}] ])
+      i.force_flush
+
+      assert !write_called
+      assert try_write_called
+
+      i.stop; i.before_shutdown; i.shutdown; i.after_shutdown; i.close; i.terminate
+    end
   end
 
   sub_test_case 'sync output feature' do
